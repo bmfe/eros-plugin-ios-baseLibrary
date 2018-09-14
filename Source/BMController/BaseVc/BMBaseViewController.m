ddab5a6a5394fcb86bf6e3dab8337714d08664ee
xionghuayu <xionghuayu@benmu-health.com>
2018-09-14 14:36:58 +0800

    optimize

--- Source/BMController/BaseVc/BMBaseViewController.m
+++ Source/BMController/BaseVc/BMBaseViewController.m
@@ -218,7 +218,7 @@
 
 - (void)dealloc
 {
-    WXLogInfo(@"dealloc >>>>>>>>>>>>>>>>>>>>>> %@",self.navigationItem.title);
+    WXLogInfo(@"dealloc >>>>>>>>>>>>>>>>>>>>>> %@",self.url.absoluteString);
     
     [_instance destroyInstance];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
